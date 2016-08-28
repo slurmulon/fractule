be1bc2d8c2b74ac7c4dc5e57e0a0af33f8aebe38
evavro <me@madhax.io>
2016-08-28 12:50:41 -0700

    :bug: fixed some typos, missing this. time for actual testing

--- src/sets/tree.js
+++ src/sets/tree.js
@@ -2,7 +2,14 @@ import AbstractFractal from './abstract'
 
 export default class TreeFractal extends AbstractFractal {
 
-  constructor () {
+  constructor ({
+    scale,
+    epsilon,
+    offset,
+    width,
+    height,
+    depth
+  }) {
     super(...arguments)
 
     this.angles = [
@@ -21,12 +28,12 @@ export default class TreeFractal extends AbstractFractal {
 
     if (depth === 0) {
       // done. draw branches.
-      drawBranch(depth, size * this.epsilon, angles[0]);
-      drawBranch(depth, size * this.epsilon, angles[1]);
+      this.drawBranch(depth, size * this.epsilon, angles[0])
+      this.drawBranch(depth, size * this.epsilon, angles[1])
     } else {
       // more. draw two mini trees instead of branches.
-      drawUnit(depth - 1, size * scaleFactor, angles[0]);
-      drawUnit(depth - 1, size * scaleFactor, angles[1]);
+      this.drawUnit(depth - 1, size * this.epsilon, angles[0])
+      this.drawUnit(depth - 1, size * this.epsilon, angles[1])
     }
   }
 

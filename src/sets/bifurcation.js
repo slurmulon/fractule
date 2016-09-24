d4246ea87dd9237cf9356effc6bf175c611980fa
evavro <me@madhax.io>
2016-09-24 12:52:22 -0700

    :art: bifurcation. should use setInterval for slicker renderings

---
+++ src/sets/bifurcation.js
@@ -0,0 +1,68 @@
+// pg. 720 - Logistic Map Bifurcation
+// pg. 687, 691 - Populations
+
+import { AbstractFractal } from '../abstract'
+
+export class BifurcationFractal extends AbstractFractal {
+
+  constructor ({
+    x, // (r)
+    y = 0.5, // (p)
+    minX,
+    minY,
+    maxX,
+    maxY,
+    width,
+    height,
+    depth
+  }) {
+    super(...arguments)
+
+    this.x = x
+    this.y = y
+    this.minX = minX
+    this.minY = minY
+    this.maxX = maxX
+    this.maxY = maxY
+    this.cursor = 0
+  }
+
+  setup () {
+    this.clear()
+
+    this.deltas = {
+      x: (this.maxX - this.minX) / this.width,
+      y: (this.maxY - this.minY) / this.height
+    }
+  }
+
+  renderUnit (cursor) {
+    const scale = this.minX + this.cursor * this.deltas.x
+
+    this.y = this.y * scale * (1 - this.y) // TODO: put this in its own func
+
+    if (cursor > 100) {
+      const x = this.cursor
+      const y = this.height - (this.y - this.minY) / this.deltas.y
+
+      console.log('\tat', x, y)
+
+      this.context.fillRect(x, y, 1, 1)
+    }
+  }
+
+  // exitUnit
+
+  iterate (depth = this.cursor, size = this.size, angle = 0) {
+    this.y = 0.5
+
+    for (let at = 0; at < 200; at += 1) {
+      this.renderUnit(at)
+    }
+
+    if (++this.cursor < this.width) {
+      this.iterate(this.cursor)
+    }
+  }
+
+}

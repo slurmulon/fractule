569cef626d47db2ca0babeae070bc6539e88e6e7
evavro <me@madhax.io>
2019-03-23 20:03:02 -0700

    :construction: :see_no_evil: Temproary scaling jankiness in LSystems (not working yet)

--- src/lsystem.js
+++ src/lsystem.js
@@ -16,8 +16,8 @@ export class LSystemFractal extends AbstractFractal {
   constructor ({
     scale,
     // dist = 10,
-    width,
-    height,
+    width = window.innerWidth,
+    height = window.innerHeight,
     depth,
     angle = 120,
     distance = 10,
@@ -28,6 +28,8 @@ export class LSystemFractal extends AbstractFractal {
     rules = {}, //[],
     iterations = 1,
     grammar = TurtleGrammar,
+    dimensions = new LSystemDimensions({}),
+    offsets = { x: 0, y: 0 },
     ceiling = 1000000000
   }) {
     super(...arguments)
@@ -40,11 +42,14 @@ export class LSystemFractal extends AbstractFractal {
     this.axiom = axiom
     this.iterations = iterations
     this.grammar = grammar
+    this.dimensions = dimensions
+    this.offsets = offsets
 
     this.cursor = null
     this.stack = []
   }
 
+  // TODO: Consider making these analogous to "points" so that they can be automagically processed by `this.iterate`
   get commands () {
     let { axiom, rules, ceiling } = this
     let commands = []
@@ -70,13 +75,42 @@ export class LSystemFractal extends AbstractFractal {
     return commands.join('')
   }
 
+  // TODO: Move a lot of this into `translate` override
+  setup () {
+    this.clear()
+
+    const { dimensions, distance, height, width } = this
+
+    let scaledDistance
+
+    if (dimensions.maxX - dimensions.minX > dimensions.maxY - dimensions.minY) {
+      scaledDistance = (width / Math.max(1, dimensions.maxX - dimensions.minX)) * distance
+    } else {
+      scaledDistance = (height / Math.max(1, dimensions.maxY - dimensions.minY)) * distance
+    }
+
+    console.log('OLD DIMENSIONS', dimensions, scaledDistance)
+
+    dimensions.minX = 0 //(scaledDistance / distance)
+    dimensions.maxX = (scaledDistance / distance)
+    dimensions.minY = 0 //(scaledDistance / distance)
+    dimensions.maxY = (scaledDistance / distance)
+
+    console.log('NEW DIMENSIONS', dimensions)
+
+    this.offsets.x = (width / 2) - (((dimensions.maxX - dimensions.minX) / 2) + dimensions.minX)
+    this.offsets.y = (height / 2) - (((dimensions.maxY - dimensions.minY) / 2) + dimensions.minY)
+
+    this.context.translate(this.offsets.x, 0)
+    this.context.strokeStyle = 'rgb(0,0,0)'
+  }
+
   draw (grammar = this.grammar, colors) {
     const { commands, angle, distance, constants } = this
     let radian, lastX, lastY
 
     this.cursor = new LSystemPosition({})
     this.stack = []
-    this.context.strokeStyle = 'rgb(0,0,0)'
 
     this.setup()
 
@@ -100,9 +134,13 @@ export class LSystemFractal extends AbstractFractal {
         console.log('cursor', this.cursor, radian)
         console.log('dims', this.height, this.width)
 
+        const yOffset = this.offsets.y //0 //1000
+
+        console.log('yoffset', yOffset)
+
         this.context.beginPath()
-        this.context.moveTo(lastX, this.height - (lastY)) //+ yOffset
-        this.context.lineTo(this.cursor.x, this.height - (this.cursor.y)) //+ yOffset
+        this.context.moveTo(lastX, this.height - (lastY + yOffset))
+        this.context.lineTo(this.cursor.x, this.height - (this.cursor.y + yOffset))
       }
     }
   }
@@ -153,6 +191,22 @@ export class LSystemPosition {
     this.y = y
     this.heading = heading
     this.color = color
+  }
+
+}
+
+export class LSystemDimensions {
+
+  constructor ({
+    minX = 0,
+    minY = 0,
+    maxX = 0,
+    maxY = 0
+  }) {
+    this.minX = minX
+    this.minY = minY
+    this.maxX = maxX
+    this.maxY = maxY
   }
 
 }
